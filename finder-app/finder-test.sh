48318bf9b71e9a4f02e8f80e807382c9b2a7de90
Zehariah Oginsky <zeog1488@colorado.edu>
2024-02-08 09:28:01 -0700

    finder tester fixes

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -54,10 +54,10 @@ fi
 
 for i in $( seq 1 $NUMFILES)
 do
-	usr/bin/writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+	writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
-OUTPUTSTRING=$(/usr/bin/finder.sh "$WRITEDIR" "$WRITESTR") >> /tmp/assignment4-result.txt
+OUTPUTSTRING=$(finder.sh "$WRITEDIR" "$WRITESTR") >> /tmp/assignment4-result.txt
 
 
 # remove temporary directories
