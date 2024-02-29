53fd7914bf705bf8fbb5f7f64c39243550e43935
Zehariah Oginsky <zeog1488@colorado.edu>
2024-02-29 14:44:21 -0700

    Assignment 6 part 1

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -12,14 +12,210 @@
 #include <errno.h>
 #include <stdbool.h>
 #include <signal.h>
+#include <pthread.h>
+#include <sys/time.h>
+#include <time.h>
+#include "queue.h"
 
 #define PORT 9000
 #define START_LEN 128
 
+typedef struct socket_thread_data_s socket_thread_data_t;
+struct socket_thread_data_s
+{
+    pthread_mutex_t *m_mutex_exit;
+    int m_conn_fd;
+    bool m_thread_complete;
+    bool m_exit_requested;
+};
+
+typedef struct slist_data_s slist_data_t;
+struct slist_data_s
+{
+    pthread_t *thread;
+    socket_thread_data_t *thread_data;
+    struct in_addr client_addr;
+    SLIST_ENTRY(slist_data_s)
+    entries;
+};
+
 int socket_fd, fd;
-char *buffer = NULL;
-char *sendBuf = NULL;
-bool isConnectionInProgress = false;
+volatile sig_atomic_t exitRequested = 0;
+bool test = false;
+pthread_mutex_t mutex;
+
+void thread_cleanup(char *buffer, char *sendBuf)
+{
+    if (buffer != NULL)
+    {
+        free(buffer);
+    }
+    if (sendBuf != NULL)
+    {
+        free(sendBuf);
+    }
+}
+
+void *process_connection(void *thread_func_data)
+{
+    socket_thread_data_t *thread_data = (socket_thread_data_t *)thread_func_data;
+
+    char *buffer = NULL;
+    char *sendBuf = NULL;
+    ssize_t len_recv, len_write, len_read;
+    int len, buflen, pos;
+    char *temp = NULL;
+    off_t fsize;
+
+    while (1)
+    {
+        buflen = START_LEN;
+        pos = 0;
+        buffer = (char *)malloc(sizeof(char) * buflen);
+        if (buffer == NULL)
+        {
+            perror("malloc");
+            printf("buffer malloc failed\n");
+            thread_cleanup(buffer, sendBuf);
+            thread_data->m_thread_complete = true;
+            return thread_data;
+        }
+        memset(buffer, 0, buflen);
+        do
+        {
+            pthread_mutex_lock(thread_data->m_mutex_exit);
+            if (thread_data->m_exit_requested)
+            {
+                thread_cleanup(buffer, sendBuf);
+                thread_data->m_thread_complete = true;
+                pthread_mutex_unlock(thread_data->m_mutex_exit);
+                return thread_data;
+            }
+            pthread_mutex_unlock(thread_data->m_mutex_exit);
+
+            len_recv = recv(thread_data->m_conn_fd, buffer + pos, buflen - pos - 1, 0);
+            if (len_recv == -1)
+            {
+                if (errno == EAGAIN || errno == EWOULDBLOCK)
+                {
+                    continue;
+                }
+                perror("recv");
+                printf("Receive failed\n");
+                thread_cleanup(buffer, sendBuf);
+                thread_data->m_thread_complete = true;
+                return thread_data;
+            }
+            else if (len_recv == buflen - pos - 1)
+            {
+                buflen += START_LEN;
+                pos = buflen - START_LEN - 1;
+                temp = (char *)realloc(buffer, buflen);
+                if (temp == NULL)
+                {
+                    perror("realloc");
+                    printf("Realloc failed\n");
+                    thread_cleanup(buffer, sendBuf);
+                    thread_data->m_thread_complete = true;
+                    return thread_data;
+                }
+                buffer = temp;
+            }
+            else if (len_recv == 0)
+            {
+                thread_cleanup(buffer, sendBuf);
+                thread_data->m_thread_complete = true;
+                return thread_data;
+            }
+            else
+            {
+                pos += len_recv;
+            }
+            *(buffer + (buflen - 1)) = 0;
+        } while (strchr(buffer, '\n') == NULL);
+
+        pthread_mutex_lock(&mutex);
+        len_write = write(fd, buffer, strlen(buffer));
+        if (len_write == -1)
+        {
+            perror("write");
+            printf("Write failed\n");
+            thread_cleanup(buffer, sendBuf);
+            thread_data->m_thread_complete = true;
+            return thread_data;
+        }
+        else if (len_write != strlen(buffer))
+        {
+            perror("write");
+            printf("Partial write occurred\n");
+            thread_cleanup(buffer, sendBuf);
+            thread_data->m_thread_complete = true;
+            return thread_data;
+        }
+        pthread_mutex_unlock(&mutex);
+
+        if (fdatasync(fd) == -1)
+        {
+            perror("fdatasync");
+            printf("fdatasync failed\n");
+            thread_cleanup(buffer, sendBuf);
+            thread_data->m_thread_complete = true;
+            return thread_data;
+        }
+
+        fsize = lseek(fd, 0, SEEK_END);
+        lseek(fd, 0, SEEK_SET);
+
+        len = fsize;
+
+        sendBuf = (char *)malloc(sizeof(char) * (len + 1));
+        if (sendBuf == NULL)
+        {
+            perror("malloc");
+            printf("sendBuf malloc failed\n");
+            thread_cleanup(buffer, sendBuf);
+            thread_data->m_thread_complete = true;
+            return thread_data;
+        }
+
+        temp = sendBuf;
+
+        while (len != 0 && (len_read = read(fd, temp, len)) != 0)
+        {
+            if (len_read == -1)
+            {
+                if (errno == EINTR)
+                    continue;
+                perror("read");
+                printf("File read failure\n");
+                thread_cleanup(buffer, sendBuf);
+                thread_data->m_thread_complete = true;
+                return thread_data;
+            }
+            len -= len_read;
+            temp += len_read;
+        }
+
+        temp = sendBuf;
+        len = fsize;
+
+        while (len != 0 && (len_write = write(thread_data->m_conn_fd, temp, len)) != 0)
+        {
+            if (len_write == -1)
+            {
+                if (errno == EINTR)
+                    continue;
+                perror("write");
+                printf("Socket write failure\n");
+                thread_cleanup(buffer, sendBuf);
+                thread_data->m_thread_complete = true;
+                return thread_data;
+            }
+            len -= len_write;
+            temp += len_write;
+        }
+    }
+}
 
 void cleanup()
 {
@@ -31,40 +227,59 @@ void cleanup()
     {
         close(fd);
     }
-    if (buffer != NULL)
-    {
-        free(buffer);
-    }
-    if (sendBuf != NULL)
-    {
-        free(sendBuf);
-    }
     remove("/var/tmp/aesdsocketdata");
     closelog();
 }
 
 static void sigint_handler(int signo)
 {
-    while (isConnectionInProgress)
-    {
-    }
     syslog(LOG_DEBUG, "Caught signal, exiting");
-    cleanup();
-    exit(EXIT_SUCCESS);
+    exitRequested = 1;
+}
+
+void append_timestamp(int signo)
+{
+    time_t t;
+    struct tm *wallTime;
+    char time_str[16];
+    char str[] = "timestamp:";
+    memset(time_str, 0, sizeof(time_str));
+
+    t = time(NULL);
+    wallTime = localtime(&t);
+    strftime(time_str, sizeof(time_str), "%Y%m%d%H%M%S", wallTime);
+    time_str[14] = '\n';
+    pthread_mutex_lock(&mutex);
+    write(fd, str, strlen(str));
+    write(fd, time_str, strlen(time_str));
+    pthread_mutex_unlock(&mutex);
 }
 
 int main(int argc, char *argv[])
 {
-    int conn_fd, len, buflen, pos;
+    int conn_fd;
     int reuseaddr = 1;
-    ssize_t len_recv, len_write, len_read;
     struct sockaddr_in server, client;
     char client_str[INET_ADDRSTRLEN];
-    char *temp = NULL;
-    off_t fsize;
     socklen_t client_size;
     bool useDaemon;
     pid_t pid;
+    slist_data_t *datap = NULL;
+    slist_data_t *datap_temp = NULL;
+    socket_thread_data_t *thread_data = NULL;
+    struct timeval ts;
+    ts.tv_sec = 0;
+    ts.tv_usec = 50000;
+    struct itimerval delay;
+    delay.it_value.tv_sec = 0;
+    delay.it_value.tv_usec = 1;
+    delay.it_interval.tv_sec = 10;
+    delay.it_interval.tv_usec = 0;
+    pthread_mutex_init(&mutex, NULL);
+
+    SLIST_HEAD(slisthead, slist_data_s)
+    head;
+    SLIST_INIT(&head);
 
     if (argc == 2)
     {
@@ -113,8 +328,18 @@ int main(int argc, char *argv[])
     if (setsockopt(socket_fd, SOL_SOCKET, SO_REUSEADDR, &reuseaddr, sizeof(int)) == -1)
     {
         perror("setsockopt");
+        printf("Couldn't set socket reusability\n");
         exit(-1);
     }
+
+    if (setsockopt(socket_fd, SOL_SOCKET, SO_RCVTIMEO, &ts, sizeof(ts)) == -1)
+    {
+        perror("setsockopt");
+        printf("Couldn't set socket timeout\n");
+        exit(-1);
+    }
+
+    fcntl(socket_fd, F_SETFL, O_NONBLOCK);
 
     memset(&server, 0, sizeof(server));
 
@@ -146,6 +371,20 @@ int main(int argc, char *argv[])
             exit(EXIT_SUCCESS);
         }
     }
+    if (signal(SIGALRM, append_timestamp) == SIG_ERR)
+    {
+        perror("signal");
+        printf("Couldn't handle SIGALRM");
+        cleanup();
+        exit(-1);
+    }
+    if (setitimer(ITIMER_REAL, &delay, NULL))
+    {
+        perror("setitimer");
+        printf("Error starting timer");
+        cleanup();
+        exit(-1);
+    }
     if ((listen(socket_fd, 10)) != 0)
     {
         perror("listen");
@@ -157,145 +396,75 @@ int main(int argc, char *argv[])
     client_size = sizeof(client);
 
     fd = open("/var/tmp/aesdsocketdata", O_RDWR | O_CREAT | O_APPEND, 0644);
-    while (1)
+    while (exitRequested == 0)
     {
         conn_fd = accept(socket_fd, (struct sockaddr *)&client, &client_size);
         if (conn_fd == -1)
         {
+            if (errno == EAGAIN || errno || EWOULDBLOCK)
+            {
+                continue;
+            }
             perror("accept");
             printf("Accept failed\n");
             cleanup();
             exit(-1);
         }
 
-        isConnectionInProgress = true;
-
         inet_ntop(AF_INET, &(client.sin_addr), client_str, INET_ADDRSTRLEN);
-
         syslog(LOG_DEBUG, "Accepted connection from %s", client_str);
 
-        buflen = START_LEN;
-        pos = 0;
-        buffer = (char *)malloc(sizeof(char) * buflen);
-        if (buffer == NULL)
+        thread_data = (socket_thread_data_t *)malloc(sizeof(socket_thread_data_t));
+        thread_data->m_conn_fd = conn_fd;
+        thread_data->m_mutex_exit = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t));
+        pthread_mutex_init(thread_data->m_mutex_exit, NULL);
+        thread_data->m_thread_complete = false;
+        thread_data->m_exit_requested = false;
+
+        datap = (slist_data_t *)malloc(sizeof(slist_data_t));
+        datap->thread = (pthread_t *)malloc(sizeof(pthread_t));
+        datap->thread_data = thread_data;
+        datap->client_addr = client.sin_addr;
+        SLIST_INSERT_HEAD(&head, datap, entries);
+
+        pthread_create(datap->thread, NULL, process_connection, (void *)thread_data);
+
+        SLIST_FOREACH_SAFE(datap, &head, entries, datap_temp)
         {
-            perror("malloc");
-            printf("buffer malloc failed\n");
-            cleanup();
-            exit(-1);
-        }
-        memset(buffer, 0, buflen);
-        do
-        {
-            len_recv = recv(conn_fd, buffer + pos, buflen - pos - 1, 0);
-            if (len_recv == -1)
+            if (datap->thread_data->m_thread_complete)
             {
-                perror("recv");
-                printf("Receive failed\n");
-                cleanup();
-                exit(-1);
+                pthread_join(*(datap->thread), NULL);
+                inet_ntop(AF_INET, &(datap->client_addr), client_str, INET_ADDRSTRLEN);
+                syslog(LOG_DEBUG, "Closed connection from %s", client_str);
+                close(datap->thread_data->m_conn_fd);
+                SLIST_REMOVE(&head, datap, slist_data_s, entries);
+                free(datap->thread_data->m_mutex_exit);
+                free(datap->thread_data);
+                free(datap->thread);
+                free(datap);
             }
-            else if (len_recv == buflen - pos - 1)
-            {
-                buflen += START_LEN;
-                pos = buflen - START_LEN - 1;
-                temp = (char *)realloc(buffer, buflen);
-                if (temp == NULL)
-                {
-                    perror("realloc");
-                    printf("Realloc failed\n");
-                    cleanup();
-                    exit(-1);
-                }
-                buffer = temp;
-            }
-            else
-            {
-                pos += len_recv;
-            }
-            *(buffer + (buflen - 1)) = 0;
-        } while (strchr(buffer, '\n') == NULL);
-
-        len_write = write(fd, buffer, strlen(buffer));
-        if (len_write == -1)
-        {
-            perror("write");
-            printf("Write failed\n");
-            cleanup();
-            exit(-1);
         }
-        else if (len_write != strlen(buffer))
-        {
-            perror("write");
-            printf("Partial write occurred\n");
-            cleanup();
-            exit(-1);
-        }
-
-        if (fdatasync(fd) == -1)
-        {
-            perror("fdatasync");
-            printf("fdatasync failed\n");
-            cleanup();
-            exit(-1);
-        }
-
-        fsize = lseek(fd, 0, SEEK_END);
-        lseek(fd, 0, SEEK_SET);
-
-        len = fsize;
-
-        sendBuf = (char *)malloc(sizeof(char) * (len + 1));
-        if (sendBuf == NULL)
-        {
-            perror("malloc");
-            printf("sendBuf malloc failed\n");
-            cleanup();
-            exit(-1);
-        }
-
-        temp = sendBuf;
-
-        while (len != 0 && (len_read = read(fd, temp, len)) != 0)
-        {
-            if (len_read == -1)
-            {
-                if (errno == EINTR)
-                    continue;
-                perror("read");
-                printf("File read failure\n");
-                cleanup();
-                exit(-1);
-            }
-            len -= len_read;
-            temp += len_read;
-        }
-
-        temp = sendBuf;
-        len = fsize;
-
-        while (len != 0 && (len_write = write(conn_fd, temp, len)) != 0)
-        {
-            if (len_write == -1)
-            {
-                if (errno == EINTR)
-                    continue;
-                perror("write");
-                printf("Socket write failure\n");
-                cleanup();
-                exit(-1);
-            }
-            len -= len_write;
-            temp += len_write;
-        }
-
-        syslog(LOG_DEBUG, "Closed connection from %s", client_str);
-
-        free(sendBuf);
-        sendBuf = NULL;
-        free(buffer);
-        buffer = NULL;
-        close(conn_fd);
-        isConnectionInProgress = false;
     }
+
+    SLIST_FOREACH(datap, &head, entries)
+    {
+        pthread_mutex_lock(datap->thread_data->m_mutex_exit);
+        datap->thread_data->m_exit_requested = true;
+        pthread_mutex_unlock(datap->thread_data->m_mutex_exit);
+    }
+
+    SLIST_FOREACH_SAFE(datap, &head, entries, datap_temp)
+    {
+        pthread_join(*(datap->thread), NULL);
+        inet_ntop(AF_INET, &(datap->client_addr), client_str, INET_ADDRSTRLEN);
+        syslog(LOG_DEBUG, "Closed connection from %s", client_str);
+        close(datap->thread_data->m_conn_fd);
+        SLIST_REMOVE(&head, datap, slist_data_s, entries);
+        free(datap->thread_data->m_mutex_exit);
+        free(datap->thread_data);
+        free(datap->thread);
+        free(datap);
+    }
+    cleanup();
+    exit(EXIT_SUCCESS);
 }
